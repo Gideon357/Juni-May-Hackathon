032499da20331d83fbe98a87df3d2f0b1a84a5e9
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-05 17:28:47 -0400

    Finish anon login logic

--- allergy_detection_app/lib/services/auth_logic.dart
+++ allergy_detection_app/lib/services/auth_logic.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:allergy_detection_app/services/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Custom User object based on the FirebaseAuth User
+  User _customUser (FirebaseUser user){
+    return user != null ? new User(uid: user.uid) : null; 
+  }
 
   // Anon sign in
   Future signinAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user =  result.user;
-      return user;
+      return _customUser(user);
 
     } catch (e) {
       print(e.toString());
