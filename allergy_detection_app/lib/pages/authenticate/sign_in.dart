d7c05737a7dfffcca8adc8e161ad5616e41239e1
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-06 11:45:13 -0400

    email signin forms. delete anon option. TODO: Update FirebaseAuth (remove anon)

--- allergy_detection_app/lib/pages/authenticate/sign_in.dart
+++ allergy_detection_app/lib/pages/authenticate/sign_in.dart
@@ -8,22 +8,24 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  final AuthService _auth =  AuthService();
+  final AuthService _auth = AuthService();
+
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[850],
+        backgroundColor: Colors.brown[100],
         appBar: AppBar(
           elevation: 0.0,
-          backgroundColor: Colors.grey[900],
+          backgroundColor: Colors.brown[400],
           title: Text(
             'Login / Sign Up',
             style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.0,
-                fontWeight: FontWeight.bold,
-                fontSize: 32.0),
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
         body: Container(
@@ -31,20 +33,44 @@ class _SignInState extends State<SignIn> {
             vertical: 20.0,
             horizontal: 50.0,
           ),
-          child: Center(
-            child: FlatButton.icon(
-              color: Colors.white,
-              textColor: Colors.black,
-              icon: Icon(Icons.person),
-              label: Text('Sign In Anonomously'),
-              onPressed: () async {
-                dynamic result = await _auth.signInAnon();
-                if (result == null){
-                  print('Error Signing In. See logs for auth_logic.dart');
-                } else{
-                  print('User logged in: ${result.uid}');
-                } 
-              },
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                      print(email);
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                      print(password);
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                RaisedButton(
+                  color: Colors.brown[400],
+                  onPressed: () async {},
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
             ),
           ),
         ));
