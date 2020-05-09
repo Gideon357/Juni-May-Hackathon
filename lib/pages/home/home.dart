3db7409119e8ea7f4a9f9f86142fe1c0610938f5
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-09 15:30:23 -0400

    Revert tab changes

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,61 +1,36 @@
 import 'package:allergy_detection_app/services/auth_logic.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:allergy_detection_app/services/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
-import '../../services/database.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:allergy_detection_app/services/database.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+class Home extends StatelessWidget {
 
-class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return StreamProvider<QuerySnapshot>.value(
       value: DatabaseService().users,
-      child: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
-              title: Text('Home'),
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.camera_alt), title: Text('Scanner'))
           ],
         ),
-        tabBuilder: (context, i) {
-          if (i == 1) {
-            return CupertinoPageScaffold(
-              child: Text('Hello, World'),
-              navigationBar: CupertinoNavigationBar(
-                  middle: Text(
-                    'Home Page',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  trailing: FlatButton.icon(
-                      onPressed: () async {
-                        await _auth.signOut();
-                      },
-                      icon: Icon(
-                        Icons.person,
-                        color: CupertinoColors.activeBlue,
-                      ),
-                      label: Text(
-                        'Log out',
-                        style: TextStyle(color: CupertinoColors.activeBlue),
-                      ))),
-            );
-          }
-        },
+        body: Container(),
       ),
     );
   }
