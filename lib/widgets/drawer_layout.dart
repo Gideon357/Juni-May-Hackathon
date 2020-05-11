88be32c626f6ffbfcd0155cb6cec552db9f68917
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-11 11:06:22 -0400

    Finish custom drawer

--- lib/widgets/drawer_layout.dart
+++ lib/widgets/drawer_layout.dart
@@ -9,95 +9,109 @@ class DrawerLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: _DrawerCliper(),
-      child: Drawer(
-        child: Container(
-          padding: EdgeInsets.only(top: 48.0),
-          child: SingleChildScrollView(
-            child: Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20, bottom: 20),
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Icon(
-                        Icons.close,
-                        color: Colors.white,
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return ClipPath(
+          clipper: _DrawerCliper(),
+          child: Drawer(
+            child: SingleChildScrollView(
+              child: Container(
+                height: (orientation == Orientation.portrait)
+                    ? MediaQuery.of(context).size.height
+                    : MediaQuery.of(context).size.width,
+                padding: EdgeInsets.only(top: 48.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20, bottom: 20),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Icon(
+                            Icons.close,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                  DrawerItem(
-                    text: 'Home',
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    },
-                  ),
-                  DrawerItem(
-                    text: 'Scanner',
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed('/scanner');
-                    },
-                  ),
-                  DrawerItem(
-                    text: 'Profile',
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed('/profile');
-                    },
-                  ),
-                  DrawerItem(
-                    text: 'App Settings',
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      Navigator.of(context).pushReplacementNamed('/scanner');
-                    },
-                  ),
-                  DrawerItem(
-                    text: 'Support',
-                    onPressed: () {},
-                  ),
-                  DrawerItem(
-                    text: 'Terms and Conditions',
-                    onPressed: () {},
-                  ),
-                  FlatButton(
-                    child: RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: 'Wrong User? ',
-                            style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.grey)),
-                        TextSpan(
-                            text: 'Sign out',
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                decoration: TextDecoration.underline))
-                      ]),
+                    DrawerItem(
+                      text: 'Home',
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      },
                     ),
-                    onPressed: () {
-                      auth.signOut();
-                    },
-                  )
-                ],
+                    DrawerItem(
+                      text: 'Scanner',
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacementNamed('/scanner');
+                      },
+                    ),
+                    DrawerItem(
+                      text: 'Profile',
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacementNamed('/profile');
+                      },
+                    ),
+                    DrawerItem(
+                      text: 'App Settings',
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        Navigator.of(context).pushReplacementNamed('/scanner');
+                      },
+                    ),
+                    DrawerItem(
+                      text: 'Support',
+                      onPressed: () {},
+                    ),
+                    DrawerItem(
+                      text: 'Terms and Conditions',
+                      onPressed: () {},
+                    ),
+                    Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: FlatButton(
+                        child: RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                                text: 'Wrong User? ',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.grey)),
+                            TextSpan(
+                                text: 'Sign out',
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    decoration: TextDecoration.underline))
+                          ]),
+                        ),
+                        onPressed: () {
+                          auth.signOut();
+                        },
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
