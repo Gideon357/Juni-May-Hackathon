008eb0a23032e6cf7c4eee5b8b6f58d8bc5f8f8d
Gideon Grinberg <grinberg1165@gmail.com>
2020-05-12 10:33:08 -0400

    Heroku stuff

--- backend/main.py
+++ backend/main.py
@@ -7,4 +7,4 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-app.run(host='localhost', port='8080')
+app.run(host='localhost', port='8080', ssl_context=('cert.pem', 'key.pem'))
